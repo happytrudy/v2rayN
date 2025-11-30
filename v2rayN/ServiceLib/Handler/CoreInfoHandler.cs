a693b44424f05e2c9461e46f217eebdfeaad3229
happytrudy <61214170+happytrudy@users.noreply.github.com>
2025-11-30 20:45:24 +0800

    add echTennel

--- v2rayN/ServiceLib/Handler/CoreInfoHandler.cs
+++ v2rayN/ServiceLib/Handler/CoreInfoHandler.cs
@@ -202,6 +202,15 @@ public sealed class CoreInfoHandler
                     AbsolutePath = false,
                 },
 
+                 new CoreInfo
+                {
+                    CoreType = ECoreType.echTunnel,
+                    CoreExes = [ "echTunnel", "echTunnel"],
+                    Arguments = "-l proxy://127.0.0.1:1080 -f wss://cloud.2027168.xyz:443/manager -token dubuxOpopop880",
+                    Url =  GetCoreUrl(ECoreType.echTunnel),
+                    AbsolutePath = false,
+                },
+
                 new CoreInfo
                 {
                     CoreType = ECoreType.shadowquic,
